0acd3a72a7b8dec5256afd9fa6f304b8b526288b
Boris Yastrebov <contact.yastrebov@gmail.com>
2019-04-15 22:27:21 +0300

    global styling and total bar styling added

--- src/components/TotalBar/TotalBar.js
+++ src/components/TotalBar/TotalBar.js
@@ -1,30 +1,65 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import styled from 'styled-components'
+
+const TotalBarWrapper = styled.div`
+  display: flex;
+  margin-bottom: 28px;
+  padding: 10px;
+  border-radius: 6px;
+  background-color: #2ecc71;
+  color: #ffffff;
+`
+
+const TotalBarTotal = styled.div`
+  display: flex;
+  flex-direction: column;
+  flex: 1 1 0;
+`
+
+const TotalBarFields = styled.div`
+  display: flex;
+  flex-direction: column;
+  flex: 1 1 0;
+`
+
+const TotalBarField = styled.div`
+  display: flex;
+  justify-content: space-around;
+`
+
+const Text = styled.p`
+  margin: 6px;
+  font-size: ${props => (props.big ? '28px' : props.medium ? '18px' : '14px')};
+  font-weight: ${props => (props.bold ? '600' : '400')};
+  text-transform: uppercase;
+`
 
 const TotalBar = props => {
   const { total, bill, friends, tips } = props.split
 
   return (
-    <div className="total-bar">
-      <div className="total-bar__total">
-        <p className="total-bar__heading">Total</p>
-        <p className="total-bar__total-value">${total}</p>
-      </div>
-      <div className="total-bar__fields">
-        <div className="total-bar__field">
-          <p className="total-bar__title">Bill</p>
-          <p className="total-bar__value">${bill}</p>
-        </div>
-        <div className="total-bar__field">
-          <p className="total-bar__title">Friends</p>
-          <p className="total-bar__value">${friends}</p>
-        </div>
-        <div className="total-bar__field">
-          <p className="total-bar__title">Tips({tips}%)</p>
-          <p className="total-bar__value">${}</p>
-        </div>
-      </div>
-    </div>
+    <TotalBarWrapper>
+      <TotalBarTotal>
+        <Text medium>Total</Text>
+        <Text big>${total}</Text>
+      </TotalBarTotal>
+
+      <TotalBarFields>
+        <TotalBarField>
+          <Text small>Bill</Text>
+          <Text small>${bill}</Text>
+        </TotalBarField>
+        <TotalBarField>
+          <Text small>Friends</Text>
+          <Text small>${friends}</Text>
+        </TotalBarField>
+        <TotalBarField>
+          <Text small>Tips({tips}%)</Text>
+          <Text small>${}</Text>
+        </TotalBarField>
+      </TotalBarFields>
+    </TotalBarWrapper>
   )
 }
 
--- src/routes/AppRouter.js
+++ src/routes/AppRouter.js
@@ -3,16 +3,37 @@ import { BrowserRouter, Route, Switch, Link, NavLink } from 'react-router-dom'
 import TotalBar from '../components/TotalBar/TotalBar'
 import EditPage from '../containers/EditPage/EditPage'
 import ResultPage from '../containers/ResultPage/ResultPage'
+import styled, { createGlobalStyle } from 'styled-components'
+import 'normalize.css'
+
+const GlobalStyle = createGlobalStyle`
+  html {
+    box-sizing: border-box;
+  }
+  *, *:before, *:after {
+    box-sizing: inherit;
+  }
+`
+
+const App = styled.div`
+  max-width: 450px;
+  margin: 50px auto;
+  padding: 20px 12px;
+  box-shadow: 3px 4px 25px -15px rgba(179, 179, 179, 1);
+  border-radius: 10px;
+`
 
 const AppRouter = () => (
   <BrowserRouter>
-    <div>
+    <App>
       <TotalBar />
       <Switch>
         <Route path="/" component={EditPage} exact={true} />
         <Route path="/result" component={ResultPage} />
       </Switch>
-    </div>
+      <Link to="/result">SPLIT BILL</Link>
+      <GlobalStyle />
+    </App>
   </BrowserRouter>
 )
 
