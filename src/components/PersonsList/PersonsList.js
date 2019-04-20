96251b6f17cadfe110705e658ce6a046e6148d74
Boris Yastrebov <contact.yastrebov@gmail.com>
2019-04-21 01:22:12 +0300

    PersonsList component remade

--- src/components/PersonsList/PersonsList.js
+++ src/components/PersonsList/PersonsList.js
@@ -1,21 +1,13 @@
 import React, { Component } from 'react'
 import Person from '../Person/Person'
 import { connect } from 'react-redux'
-import { addPerson, deletePersons } from '../../actions/persons'
-import {
-  getPersonsQuantity,
-  getLastPersonId,
-  getDividedTotal
-} from '../../selectors/'
 
 class PersonsList extends Component {
   render() {
     return (
       <div>
-        {this.props.friendsArr.map(item => (
-          <Person amount={this.props.dividedTotal}>
-            {this.props.dividedTotal}
-          </Person>
+        {this.props.persons.map(person => (
+          <Person amount={person.amount} />
         ))}
       </div>
     )
@@ -24,25 +16,8 @@ class PersonsList extends Component {
 
 const mapStateToProps = state => {
   return {
-    friendsArr: state.persons,
-    friends: getPersonsQuantity(state),
-    latestId: getLastPersonId(state),
-    dividedTotal: getDividedTotal(state)
+    persons: state.persons
   }
 }
 
-const mapDispatchToProps = dispatch => {
-  return {
-    addPerson: (id, amount) => {
-      dispatch(addPerson(id, amount))
-    },
-    deletePersons: () => {
-      dispatch(deletePersons())
-    }
-  }
-}
-
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(PersonsList)
+export default connect(mapStateToProps)(PersonsList)
