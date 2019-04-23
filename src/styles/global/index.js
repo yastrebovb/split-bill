6f55066c6e58f607d45e81abd675de81479b4aff
Boris Yastrebov <contact.yastrebov@gmail.com>
2019-04-23 22:18:31 +0300

    adaptive layour for mobile

--- src/styles/global/index.js
+++ src/styles/global/index.js
@@ -1,15 +1,26 @@
 import styled, { createGlobalStyle } from 'styled-components'
+import media from '../templates/'
 
 export const GlobalStyle = createGlobalStyle`
   @import url('https://fonts.googleapis.com/css?family=Open+Sans:400,700');
 
   html {
+    height: 100%;
     box-sizing: border-box;
     font-family: 'Open Sans', sans-serif;
   }
 
+  html, body, #root{
+    height: 100%
+  }
+
   *, *:before, *:after {
     box-sizing: inherit;
+  }
+
+  #root {
+    display: flex;
+    flex-direction: column;
   }
 
   @keyframes zoomInFade {
@@ -24,7 +35,7 @@ export const GlobalStyle = createGlobalStyle`
     }
 
     100% {
-      transform: scale(1);
+      transform: scale(1);  
       opacity: 1;
     }
   }
@@ -41,10 +52,19 @@ export const GlobalStyle = createGlobalStyle`
 `
 
 export const App = styled.div`
-  max-width: 450px;
-  margin: 12px auto;
+  width: 450px;
+  margin: auto;
   padding: 20px 12px;
   box-shadow: 3px 4px 25px -15px rgba(179, 179, 179, 1);
   border-radius: 10px;
   overflow: hidden;
+
+  ${media.phone`
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+    width: 100%; 
+    max-width: 100%;
+    height: 100%;
+  `}
 `
